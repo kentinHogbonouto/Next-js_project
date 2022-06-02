166c944321e48bc29e2b175a66d843c3a2e1b55f
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-06-02 11:44:27 +0000

    Set up the sign in component

---
+++ next-js_authentication_systhem/src/components/auth/login/index.js
@@ -0,0 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const Login = () => {
+  const data = useSession();
+  if (session) {
+    return (
+      <>
+        <h1>Hello world</h1>
+        <button onClick={signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      <button onClick={signIn()}>Sign in</button>
+    </>
+  );
+};
+
+export default Login;
