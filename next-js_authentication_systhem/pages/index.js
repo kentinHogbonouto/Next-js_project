5f802b9d525704bf399bb9dff75a2d3cd78e3eb2
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-06-06 21:31:42 +0000

    Tried to fixe some bugs and add sign out methode

--- next-js_authentication_systhem/pages/index.js
+++ next-js_authentication_systhem/pages/index.js
@@ -1,16 +1,23 @@
 import React, {Fragment} from "react"
-import { useSession, signIn } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home = () => {
   const {data: session} = useSession();
   return(
     <React.Fragment>
-      <h1>Welcome to my website, feel free to login using one of the ways available to you !</h1>
-
-      {session ? <h1>You are sign in</h1>: <>
-        <h1>Not sign in yet</h1>
-        <button onClick={signIn}>Sign in right now</button>
-      </>}
+      <h1>Welcome to my website</h1>
+      {console.log(session)}
+      {
+        session ? 
+        <>
+          <h1>Welcome {session.user.name}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </>: 
+        <>
+          <h1>Not sign in yet</h1>
+          <button onClick={signIn}>Sign in right now</button>
+        </>
+      }
     </React.Fragment>
   )
 }
